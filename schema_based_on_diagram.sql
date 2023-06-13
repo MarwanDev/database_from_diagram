bc9f3f5043a727d4b78416f3c2860746fecb5890
MarwanDev <marwaneg@aucegypt.edu>
2023-06-13 15:30:31 +0300

    Create database

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,42 @@
+create table medical_history (
+    id int primary key GENERATED ALWAYS AS IDENTITY,
+    admitted_at timestamp,
+    status varchar(250)
+);
+
+create table patients (
+    id int primary key GENERATED ALWAYS AS IDENTITY,
+    date_of_birth date,
+    name varchar(250)
+);
+
+alter table medical_history 
+add column patient_id int;
+
+alter table medical_history 
+add foreign key(patient_id)
+references patients(id);
+
+
+create table invoices (
+    id int primary key GENERATED ALWAYS AS IDENTITY,
+    total_amoun decimal,
+    generated_at timestamp,
+    paid_at timestamp,
+	medical_history_id int
+);
+
+alter table invoices
+add foreign key(medical_history_id)
+references medical_history(id);
+
+
+
+create table invoice_items (
+    id int primary key GENERATED ALWAYS AS IDENTITY,
+    unit_price decimal,
+	quantity int,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int
+);
